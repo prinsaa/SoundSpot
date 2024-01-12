eb2822df590903445fc82380836361f3cd79d6f5
prinsaa <prinsaptl@outlook.com>
2024-01-12 17:12:59 -0500

    change key access

--- src/createClient.js
+++ src/createClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl  = import.meta.env.VITE_APP_SUPABASE_URL
+const supabaseKey  = import.meta.env.VITE_APP_SUPABASE_KEY
 
 //connecting to server
-export const supabase=createClient(
-    'https://asmfkhzkfgodylnckimn.supabase.co',
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzbWZraHprZmdvZHlsbmNraW1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5MTA1MTIsImV4cCI6MjAxNTQ4NjUxMn0.Xk1NmGm0uPpf69_Fb5W13fpGhiOmYaJckG5FuQ8Yq1M")
+export const supabase = createClient(supabaseUrl, supabaseKey)
